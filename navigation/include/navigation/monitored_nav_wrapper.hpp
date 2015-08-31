45e860d77a81a7d17238fb671b5c290d2e1a7d21
GreatAlexander <alex.bordallo@gmail.com>
2015-08-31 15:07:14 +0100

    Implement monitored nav action client, feedbackCB and PersonPoseCB

--- navigation/include/navigation/monitored_nav_wrapper.hpp
+++ navigation/include/navigation/monitored_nav_wrapper.hpp
@@ -11,6 +11,18 @@
 #define MONITORED_NAV_WRAPPER_HPP
 
 #include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+
+#include <navigation/StartHumanApproach.h>
+
+#include <geometry_msgs/PoseStamped.h>
+
+#include <strands_navigation_msgs/MonitoredNavigationAction.h>
+#include <strands_navigation_msgs/MonitoredNavigationActionGoal.h>
+#include <strands_navigation_msgs/MonitoredNavigationActionFeedback.h>
+
+// typedef actionlib::SimpleActionClient<strands_navigation_msgs::MonitoredNavigationAction>
+// Client;
 
 class MonitoredNavWrapper {
  public:
@@ -21,15 +33,32 @@ class MonitoredNavWrapper {
   void init();
   void rosSetup();
 
+  void personPoseCB(const geometry_msgs::PoseStamped::ConstPtr& msg);
+  void feedbackCB(const strands_navigation_msgs::
+                  MonitoredNavigationActionFeedback::ConstPtr& msg);
+  bool startHumanApproach(
+    navigation::StartHumanApproach::Request& req,
+    navigation::StartHumanApproach::Response& res);
+  // void calcSafePose();
+  void sendNavGoal();
+
  private:
   // Flags
 
   // Constants
+  float personal_space_;
 
   // Variables
+  geometry_msgs::PoseStamped person_pose_;
+  geometry_msgs::PoseStamped safe_pose_;
 
   // ROS
   ros::NodeHandle* nh_;
+  ros::ServiceServer start_human_approach_;
+  ros::Subscriber person_pose_sub_;
+  ros::Subscriber monitored_feedback_sub_;
+  actionlib::SimpleActionClient<strands_navigation_msgs::MonitoredNavigationAction>
+  monitored_nav_ac;
 
 };
 
--- navigation/src/monitored_nav_wrapper.cpp
+++ navigation/src/monitored_nav_wrapper.cpp
@@ -8,8 +8,8 @@
 
 #include <navigation/monitored_nav_wrapper.hpp>
 
-MonitoredNavWrapper::MonitoredNavWrapper(ros::NodeHandle* nh) {
-  nh_ = nh;
+MonitoredNavWrapper::MonitoredNavWrapper(ros::NodeHandle* nh) : nh_(nh),
+  monitored_nav_ac("monitored_navigation", true) {
   this->loadParams();
   this->init();
   this->rosSetup();
@@ -19,10 +19,50 @@ MonitoredNavWrapper::~MonitoredNavWrapper() {
 }
 
 void MonitoredNavWrapper::loadParams() {
+  ros::param::param("personal_space", personal_space_, 1.2f);
+  ros::param::set("/move_base/DWAPlannerROS/xy_goal_tolerance", personal_space_);
 }
 
 void MonitoredNavWrapper::init() {
 }
 
 void MonitoredNavWrapper::rosSetup() {
+  person_pose_sub_ = nh_->subscribe("/people_tracker/pose", 1,
+                                    &MonitoredNavWrapper::personPoseCB, this);
+  monitored_feedback_sub_ = nh_->subscribe("/monitored_navigation/feedback", 1,
+                                           &MonitoredNavWrapper::feedbackCB, this);
+  start_human_approach_ =
+    nh_->advertiseService("start_human_approach",
+                          &MonitoredNavWrapper::startHumanApproach, this);
+  ROS_INFO("Waiting for Monitored Nav Action Server...");
+  monitored_nav_ac.waitForServer();
+  ROS_INFO("Monitored Nav Action Server ready!");
+}
+
+void MonitoredNavWrapper::personPoseCB(const
+                                       geometry_msgs::PoseStamped::ConstPtr& msg) {
+  person_pose_ = *msg;
+}
+
+void MonitoredNavWrapper::feedbackCB(const
+                                     strands_navigation_msgs::
+                                     MonitoredNavigationActionFeedback::
+                                     ConstPtr& msg) {
+  // person_pose_ = *msg;
+}
+
+bool MonitoredNavWrapper::startHumanApproach(
+  navigation::StartHumanApproach::Request& req,
+  navigation::StartHumanApproach::Response& res) {
+  res.ok = true;
+  // person_pose_ = req.person_pose;
+  this->sendNavGoal();
+  return true;
+}
+
+void MonitoredNavWrapper::sendNavGoal() {
+  strands_navigation_msgs::MonitoredNavigationGoal goal;
+  goal.action_server = "monitored_navigation";
+  goal.target_pose = person_pose_;
+  monitored_nav_ac.sendGoal(goal);
 }
